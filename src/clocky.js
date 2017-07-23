e99acb83a242026b19726831e3a97674c1bdaa55
Nathan Icart <nathan.icart@gmail.com>
2017-07-23 16:49:32 -0500

    added clock, improved style

--- src/clocky.js
+++ src/clocky.js
@@ -1,5 +1,31 @@
 window.onload = function() {
 
-  
+  var startTime = () => {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    h = twelveHourTime(h);
+    document.getElementById('clock-display').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+
+  var checkTime = (x) => {
+    if (x < 10) {
+      x = "0" + x;    
+    }
+    return x;
+  }
+
+  var twelveHourTime = (x) => {
+    if (x > 12) {
+      x = x - 12;
+    }
+    return x;
+  }
+
+  startTime();
 
 }
